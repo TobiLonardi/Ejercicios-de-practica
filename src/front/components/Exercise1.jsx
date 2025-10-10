86129263fca744ee67353f08ba3ac28ccab838a3
Tobias Lonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-10-10 16:01:49 +0000

    front basico completado sin backend

--- src/front/components/Exercise1.jsx
+++ src/front/components/Exercise1.jsx
@@ -5,36 +5,54 @@ const defaultValue = {
     "id": 0,
     "name": "",
     "email": ""
-    }
+}
 export const Exercise1 = () => {
 
     const [input, setinput] = useState(defaultValue);
     const [users, setUsers] = useState([]);
 
-    useEffect(()=>{
-        getAllUsers()
-    ,[]})
+    const url = import.meta.env.VITE_BACKEND_URL
 
-    const handleSubmit=(event)=>{
+    useEffect(() => {
+        getAllUsers()
+            , []
+    })
+
+    const handleSubmit = (event) => {
         event.preventDefault();
         //postUser
         setinput(defaultValue)
-        
+        console.log("submited")
+
+    }
+
+    function handleKeyDown(event){
+        if(event.key === "Enter"){
+            handleSubmit(event)
+        }
+    }
+
+    function onChange(event) {
+        setinput({
+            ...input,
+            [event.target.name]: event.target.value
+        }
+        )
     }
 
 
-    async function getAllUsers(){
+    async function getAllUsers() {
 
     }
 
     return (
         <div className="body d-flex flex-column justify-content-center align-items-center">
             <h1>My Users</h1>
-            <form action="" className="w-50 shadow-sm p-1 mb-5 bg-body-tertiary rounded" onSubmit={handleSubmit}>
+            <form action="" className="w-50 shadow-sm p-1 mb-5 bg-body-tertiary rounded">
                 <div className="mb-3">
-                    <input type="text" value={input.name} placeholder="Username" id="exampleName" name="name" />
+                    <input type="text" value={input.name} placeholder="Username" id="exampleName" name="name" onChange={onChange} onKeyDown={handleKeyDown}/>
                     <br />
-                    <input type="text" value={input.email} placeholder="Email" id="exampleEmail" name="email" className="mt-2"/>
+                    <input type="text" value={input.email} placeholder="Email" id="exampleEmail" name="email" className="mt-2" onChange={onChange} onKeyDown={handleKeyDown}/>
                     <ul>
                         {
                             users.map((item) =>
