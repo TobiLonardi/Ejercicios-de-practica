b73e4d28c677364e9fce855e3e270059607cf784
Tobias Lonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-10-17 15:49:49 +0000

    order Model added

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
 
@@ -8,6 +9,7 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     name: Mapped[str] = mapped_column(String(120), nullable=False)
+    order: Mapped[List["Order"]] = relationship(back_populates="user")
 
 
     def serialize(self):
@@ -16,4 +18,18 @@ class User(db.Model):
             "email": self.email,
             "name": self.name
             # do not serialize the password, its a security breach
+        }
+
+class Order(db.Model):
+    id:Mapped[int] = mapped_column(primary_key=True)
+    product: Mapped[str] = mapped_column(String(180), nullable=False)
+    amount: Mapped[int] = mapped_column(Integer, nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    user: Mapped["User"] = relationship(back_populates="order")
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "product": self.product,
+            "amount": self.amount
         }
